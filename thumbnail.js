81c67a4cc14cc75d2db444f66062eae03984add0
woxunta <86756659+woxunta@users.noreply.github.com>
2021-07-06 16:40:31 +0800

    Add files via upload

--- thumbnail.js
+++ thumbnail.js
@@ -4,8 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     targetImage.addEventListener("click", function() {
         if (targetImage.classList.contains("small")) {
             targetImage.classList.remove("small");
-        } else {
-            targetImage.classList.add("small");
+        }
         }
 
     });
