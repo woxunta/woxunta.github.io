8a026ebc968e2ced1a27466d64a328d93e329b6e
woxunta <86756659+woxunta@users.noreply.github.com>
2021-07-06 15:24:41 +0800

    Add files via upload

--- thumbnail.js
+++ thumbnail.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    alert("Hello JavaScript");
+    alert("let targetImage = document.querySelector("#smart-image");
+  targetImage.addEventListener
+  (
+    "click", function() 
+    { 
+      if (targetImage.classList.contains("small")) 
+      {
+        targetImage.classList.remove("small"); 
+      } 
+     
+    }
+  );
+  
+  
+});");
 });
